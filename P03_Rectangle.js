a02fc3dbe34263dfc078207bc8c452f00fdf8575
abhishek2427 <77685818+abhishek2427@users.noreply.github.com>
2023-07-03 05:29:58 +0530

    P03_Rectangle.js

---
+++ P03_Rectangle.js
@@ -0,0 +1,7 @@
+3.WAP to print area and perimeter of rectangle , taking input from the user , using prompt
+
+var l=parseInt(prompt("Length: "));
+var b=parseInt(prompt("Breadth: "));
+var area=l*b;
+var peri=2*(l+b);
+console.log(`Area of Rectangle is ${area} and Perimeter of Rectangle is ${peri}`);
